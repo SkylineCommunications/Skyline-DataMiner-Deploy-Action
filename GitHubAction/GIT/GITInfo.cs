974b7a6255e88e37b09007d3d682e208ae3c0673
Jan Staelens <jan.staelens@skyline.be>
2023-06-16 15:01:57 +0200

    trim away the trailing slash for libgit

--- GitHubAction/GIT/GITInfo.cs
+++ GitHubAction/GIT/GITInfo.cs
@@ -1,12 +1,15 @@
 ﻿using LibGit2Sharp;
+using LibGit2Sharp.Handlers;
+
+using System.Net;
 
 namespace GIT
 {
     public class GITInfo : IGITInfo
-    {
+    {   
         public string GetCurrentBranch()
         {
-            using (Repository localRepo = new Repository(Directory.GetCurrentDirectory()))
+            using (Repository localRepo = new Repository(Directory.GetCurrentDirectory().TrimEnd('/')))
             {
                 var thisBranch = localRepo.Head;
                 return thisBranch.FriendlyName;
@@ -15,7 +18,7 @@ namespace GIT
 
         public string GetSourceUrl()
         {
-            using (Repository localRepo = new Repository(Directory.GetCurrentDirectory()))
+            using (Repository localRepo = new Repository(Directory.GetCurrentDirectory().TrimEnd('/')))
             {
                 var remoteURL = localRepo.Network.Remotes.First().Url;
                 return remoteURL;
